8a8bdbd2cbbf97764617fb43e13fa8c35ccff53a
abu_qaanitah <mp@abuqaanitahmac.local>
2025-08-10 13:41:05 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//   theme: {
+//           extend: {
+//             colors: {
+//               primary: '#1d4ed8',
+//               secondary: '#ea580c',
+//               accent: '#dc2626',
+//             }
+//           }
+//         }
+// }
+
+// module.exports = {
+// 	content: ["./**/*.html"],
+// 	theme: {
+// 		extend: {
+// 			colors: {
+// 				primary: '#1d4ed8',
+// 				secondary: '#ea580c',
+// 				accent: '#dc2626',
+// 			},
+// 		},
+// 	},
+// 	plugins: [],
+// };
